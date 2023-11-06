660d31f481174f07171eb617f7c77f36f62a096a
Dev <hoangtv1809@gmail.com>
2023-11-06 21:16:55 +0700

    update Admin Layout

--- admin/public/footer.php
+++ admin/public/footer.php
@@ -1,1 +1,7 @@
+</div>
+
+    <script src="orders.js"></script>
+    <script src="index.js"></script>
+</body>
+
 </html>
--- admin/public/header.php
+++ admin/public/header.php
@@ -5,6 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="layout/style.css">
     <title>Responsive Dashboard Design #1 | AsmrProg</title>
 </head>
+
+<body>
+
+    <div class="container">
